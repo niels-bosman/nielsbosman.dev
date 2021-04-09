0498ea29ab4983a2eb00ddda7ca3dc40039e4d97
nehlis <nielsbosman@mail.com>
2021-04-09 09:16:59 +0200

    Cleanup files.

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script>
-import Hero from "./components/Hero";
+import Hero from './components/Hero'
 
 export default {
   name: 'App',
@@ -15,12 +15,12 @@ export default {
 
 <style>
 
+@import url('https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap');
+
 body {
   margin: 0;
   padding: 0;
 }
-
-@import url('https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap');
 
 h1, h2, h3, h4, h5, h6, p, a {
   font-family: 'Lato', sans-serif;
