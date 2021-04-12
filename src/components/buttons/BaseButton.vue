0fac0e58b06112830776dce62de291568f5127bb
nehlis <nielsbosman@mail.com>
2021-04-12 14:03:48 +0200

    Formatting

--- src/components/buttons/BaseButton.vue
+++ src/components/buttons/BaseButton.vue
@@ -1,8 +1,8 @@
 <template>
 
-    <button class="button">
-      <slot/>
-    </button>
+  <button class="button">
+    <slot/>
+  </button>
 
 </template>
 
@@ -10,7 +10,7 @@
 export default {
   name: 'Button',
   props: {
-    title: {type: String, required: true}
+    title: { type: String, required: true }
   }
 }
 </script>
